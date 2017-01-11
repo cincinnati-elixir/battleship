4c575e0590b372696f3fb6b30004d8741f1a4121
Jason Voegele <jason.voegele@gmail.com>
2017-01-11 09:55:19 -0500

    Add CLI commands for starting players and tournaments

--- lib/battleship/cli.ex
+++ lib/battleship/cli.ex
@@ -15,13 +15,13 @@ defmodule Battleship.CLI do
   end
 
   defp execute(["start-player" | args], options) do
-    IO.puts("Starting player with args = #{IO.inspect args}, options = #{IO.inspect options}")
+    start_player(args)
   end
   defp execute(["start-game" | args], options) do
     start_game(args)
   end
   defp execute(["start-tournament" | args], options) do
-    IO.puts("Starting tournament with args = #{IO.inspect args}, options = #{IO.inspect options}")
+    start_tournament(args)
   end
   defp execute(_args, _options) do
     IO.puts(:stderr, usage())
@@ -39,6 +39,25 @@ defmodule Battleship.CLI do
   end
   defp start_game(_invalid_args) do
     IO.puts(:stderr, "Usage: battleship start-game <Player1 Module> <Player2 Module>")
+  end
+
+  defp start_tournament([player1, player2]) do
+    IO.puts("Starting tournament with #{IO.inspect player1} and #{IO.inspect player2}")
+    Battleship.Game.Supervisor.start_tournament(player1, player2)
+  end
+  defp start_tournament(_invalid_args) do
+    IO.puts(:stderr, "Usage: battleship start-tournament <Player1 Module> <Player2 Module>")
+  end
+
+  defp start_player([player_module_arg]) do
+    IO.puts("Starting player #{IO.inspect player_module_arg}")
+    player_module_name = "Elixir." <> player_module_arg
+    player_module = String.to_atom(player_module_name)
+    player_init = apply(player_module, :init, [:hello])
+    IO.inspect(player_init)
+  end
+  defp start_player(_invalid_args) do
+    IO.puts(:stderr, "Usage: battleship start-player <Player Module>")
   end
 
   defp wait_for_game_over(timeout \\ 60_000) do
